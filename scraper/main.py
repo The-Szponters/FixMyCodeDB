78920ed89f1f3fc016ff189dba0ab60cff8e88de
dominik <01197296@pw.edu.pl>
2025-11-30 21:06:46 +0100

    lint: fix in main.py

--- scraper/main.py
+++ scraper/main.py
@@ -1,12 +1,9 @@
-import time
 from scraper.network.server import start_server
 from scraper.core.engine import run_scraper
-from scraper.config.scraper_config import ScraperConfig, RepoConfig
 
 
 def main():
     start_server(run_scraper)
-    # run_scraper("./scraper/config.json")
 
 
 if __name__ == "__main__":
