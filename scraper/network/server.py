809ed05f98b322c403bf9b12d07085b8d890d49d
dominik <01197296@pw.edu.pl>
2025-11-30 22:16:01 +0100

    fix: bandit error

--- scraper/network/server.py
+++ scraper/network/server.py
@@ -5,7 +5,8 @@ from typing import Any, Callable
 def start_server(callback: Callable[[str], Any]) -> None:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('0.0.0.0', 60000))
+    # nosec B104: Binding to all interfaces is required for Docker networking
+    sock.bind(('0.0.0.0', 60000))  # nosec B104
     sock.listen()
 
     print("TCP Server listening...")
@@ -22,12 +23,10 @@ def start_server(callback: Callable[[str], Any]) -> None:
             if decoded_data.startswith("SCRAPE "):
                 filename = decoded_data[7:]
                 response = f"ACK: Scraping {filename}".encode()
-                print("sent1")
                 conn.sendall(response)
                 callback(filename)
                 response = f"ACK: Finished Scraping {filename}".encode()
                 conn.sendall(response)
-                print("sent2")
             else:
                 response = b"ERROR: Invalid format. Use: SCRAPE {filename}"
                 conn.sendall(response)
